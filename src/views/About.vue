95bb4a988ebe63dd1fa4c034115970afc0cbe5ef
fukamizu <dipmizu@gmail.com>
2020-05-05 16:27:06 +0900

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
 
-    <v-carousel cycle height="500" hide-delimiter-background show-arrows-on-hover interval="3">
+    <v-carousel cycle height="500" hide-delimiter-background show-arrows-on-hover interval="3000">
       <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" 
         gradient="to bottom, rgba(20,20,20,0.8),rgba(200,200,200,.0), rgba(20,20,20,0.8)">
 
